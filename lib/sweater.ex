1e960b1ce7b4fdf94874179e37df46fa90cf046d
richardarthurbrown <brownra04@gmail.com>
2021-07-29 21:20:58 -0500

    Use forecast method with options, set up handling response

--- lib/sweater.ex
+++ lib/sweater.ex
@@ -1,8 +1,22 @@
 defmodule Sweater do
   alias ExOwm
 
+  @options [
+    cnt: 3,
+    units: :imperial
+  ]
+
   def get_weather(city) do
-    ExOwm.get_current_weather([%{city: city}])
+    ExOwm.get_five_day_forecast([%{city: city}], @options)
+    |> handle_response()
+  end
+
+  defp handle_response([ok: response]) do
+    response
+  end
+
+  defp handle_response([{:error, message, response}]) do
+    "Error getting weather"
   end
 
 end
